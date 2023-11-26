baf5dd91521511c73a0e4f3be090ecc3b4d27f5b
Kiruhin MS <kiruhin7220@gmail.com>
2023-11-26 21:46:13 +0300

    fix: add configs and fix card

--- src/components/card-list/card-list.tsx
+++ src/components/card-list/card-list.tsx
@@ -1,66 +1,68 @@
-import React, { useState } from "react";
-import { cards } from "../../api/mock/mock";
-import { Card } from "../card";
-import styled from "@emotion/styled";
-import { Pagination } from "@mui/material";
-import { Sort } from "../sort";
+import React, { useState } from 'react'
+import { cards } from '../../api/mock/mock'
+import { Card } from '../card'
+import styled from '@emotion/styled'
+import { Pagination } from '@mui/material'
+import { Sort } from '../sort'
 
 interface CardListProps {
-  search: string;
+	search: string
 }
 
 export const CardList = (props: CardListProps) => {
-  const { search } = props;
+	const { search } = props
 
-  const [page, setPage] = useState<number>(1);
-  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
-    setPage(value);
-  };
+	const [page, setPage] = useState<number>(1)
+	const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+		setPage(value)
+	}
 
-  const cardWithSearch = cards.filter((item, index) =>
-    item.name.toLowerCase().includes(search)
-  );
+	const cardWithSearch = cards.filter((item) =>
+		item.name.toLowerCase().includes(search)
+	)
 
-  const paginationCard = cardWithSearch.filter(
-    (item, index) => index >= (page - 1) * PAGE_SIZE && index < page * PAGE_SIZE
-  );
+	const paginationCard = cardWithSearch.filter(
+		(item, index) => index >= (page - 1) * PAGE_SIZE && index < page * PAGE_SIZE
+	)
 
-  return (
-    <Wrapper>
-      <div>
-        <Sort />
-        <CardsWrapper>
-          {paginationCard.map((item) => (
-            <Card
-              key={item._id}
-              pictures={item.pictures}
-              discount={item.discount}
-              price={item.price}
-              name={item.name}
-            />
-          ))}
-        </CardsWrapper>
-        <Pagination
-          count={cardWithSearch.length / PAGE_SIZE}
-          size="small"
-          page={page}
-          onChange={handleChange}
-        />
-      </div>
-    </Wrapper>
-  );
-};
+	return (
+		<Wrapper>
+			<Sort />
+			<CardsWrapper>
+				{paginationCard.map((item) => (
+					<Card
+						key={item._id}
+						pictures={item.pictures}
+						discount={item.discount}
+						price={item.price}
+						name={item.name}
+						wight={item.wight}
+					/>
+				))}
+			</CardsWrapper>
+			<Pagination
+				count={cardWithSearch.length / PAGE_SIZE}
+				size='small'
+				page={page}
+				onChange={handleChange}
+			/>
+		</Wrapper>
+	)
+}
 
 const Wrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
-  padding-bottom: 20px;
-`;
+	display: flex;
+	justify-content: center;
+	align-content: space-around;
+	flex-wrap: wrap;
+	flex-direction: column;
+	padding-bottom: 20px;
+	gap: 20px;
+`
 
 const CardsWrapper = styled.div`
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-`;
+	display: grid;
+	grid-template-columns: repeat(4, 1fr);
+`
 
-const PAGE_SIZE = 8;
+const PAGE_SIZE = 8
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
-import { createRoot } from 'react-dom/client';
-import { App } from './app';
-import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client'
+import { App } from './app'
+import { StrictMode } from 'react'
+import 'normalize.css'
 
-const domNode = document.getElementById('root') as HTMLDivElement;
-const root = createRoot(domNode);
+const domNode = document.getElementById('root') as HTMLDivElement
+const root = createRoot(domNode)
 root.render(
 	<StrictMode>
 		<App />
 	</StrictMode>
-);
+)
